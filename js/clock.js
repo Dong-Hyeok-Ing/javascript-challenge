907147a442c3f2c0a286a97bef91f92e1c119894
Dong-Hyeok-Ing <kdhhdk83@gmail.com>
2024-04-11 07:50:08 +0900

    Javascript Challenge
    javascript mission 참여를 위한 리포지토리

---
+++ js/clock.js
@@ -0,0 +1,41 @@
+const dateElement = document.getElementById("date");
+const timeElement = document.getElementById("time");
+const spanElement = timeElement.querySelector("span");
+
+const modifyNumber = (number) => {
+    return parseInt(number) < 10 ? "0"+number : number;
+}
+
+
+const getNowDate = () => {
+    const nowDate = new Date();
+    const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'];
+    let month = modifyNumber(nowDate.getMonth() +1);
+    let date = modifyNumber(nowDate.getDate());
+    let day = nowDate.getDay();
+
+    setNowDate(month, date, week[day]);
+}
+
+const setNowDate = (month, date, day) => {
+    dateElement.textContent = `${month}월 ${date}일 ${day}`
+}
+
+function getNowTime() {
+    var date = new Date();
+    let hr = modifyNumber(date.getHours());
+    let min = modifyNumber(date.getMinutes());
+    let sec = date.getSeconds();
+
+    setNowTime(hr, min, sec);
+}
+
+const setNowTime = (hr, min, sec) => {
+    timeElement.textContent = `${hr} : ${min}`; 
+    spanElement.textContent = `${sec}`;
+    timeElement.append(spanElement);
+}
+
+getNowDate();
+getNowTime();
+setInterval(getNowTime, 1000);
